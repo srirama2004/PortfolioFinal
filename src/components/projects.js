458c0766af921bed07945537c388e057ffc00f5f
srirama <sriramavate@gmail.com>
2025-01-17 22:09:42 +0530

    fina

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './projects.css';
 import resortManagementImage from './pic/img1.jpeg'; // Adjust the filename to match your actual image
 import icuAdmissionImage from './pic/img2.jpeg'; // Adjust the filename to match your actual image
-
+import hosimage from './pic/hos.jpg'; // Adjust the filename to match your actual image
+import textimage from './pic/text.png';
 const Projects = () => {
   const projects = [
     {
@@ -24,31 +25,31 @@ const Projects = () => {
         description: 'A Secure Text Storage System, where users can store and retreive text files safely. Rsa encryption, Hashing and digital signatures are used to ensure safety.',
         technologies: 'Flask, PostgreSQL, RSA encryption',
         link: 'https://github.com/srirama2004/SriHomeRepo/tree/main/Projects_sri/Ml_projects/ICUadmissionPredictor', // Placeholder if not live yet
-        image: icuAdmissionImage, // Use the imported image
+        image: textimage, // Use the imported image
       },
       {
         title: 'Beta Medical Service ',
         description: 'Web app integrated with ML models to predict blood donations. Features include patient appointments, test results, and blood donation predictions.',
         technologies: 'Machine Learning, Python, TensorFlow',
         link: 'https://github.com/srirama2004/SriHomeRepo/tree/main/Projects_sri/Ml_projects/ICUadmissionPredictor', // Placeholder if not live yet
-        image: icuAdmissionImage, // Use the imported image
+        image: hosimage, // Use the imported image
       },
   ];
 
   return (
-    <div className="parent-container">
+    <div className="parent1-container">
       {/* 3D Carousel Section */}
-      <div className="carousel-container">
-        <div className="carousel">
-        <div className="carousel">
+      <div className="carousel-containerp">
+        <div className="carouselp">
+        <div className="carouselpp">
   {projects.map((project, index) => (
-    <div className="card-container" key={index}>
-      <div className="card">
-        <div className="card-content">
-          <div className="card-image">
+    <div className="card-containerp" key={index}>
+      <div className="cardp">
+        <div className="card-contentp">
+          <div className="card-imagep">
             <img src={project.image} alt={project.title} />
           </div>
-          <div className="card-description">
+          <div className="card-descriptionp">
             <h3>{project.title}</h3>
             <p>{project.description}</p>
             <p><strong>Technologies Used:</strong> {project.technologies}</p>
