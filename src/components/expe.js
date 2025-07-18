15ed7d0e228e85356de7c73a056faed8855aa46b
srirama <sriramavate@gmail.com>
2025-07-18 18:40:13 +0530

    Update expe.js

--- src/components/expe.js
+++ src/components/expe.js
@@ -27,34 +27,6 @@ const Experience = () => {
           </ul>
         </p>
       </div>
-      <div className="courses-section">
-        <h3>Courses</h3>
-        <ul>
-        <li>
-    Exploratory Data Analysis for Machine Learning - <strong>IBM</strong>
-    <a href="https://coursera.org/share/2716363f16da8098a3041263ca7b8879" target="_blank" class="certificate-link">Certificate</a>
-  </li>
-  <li>
-    Supervised Machine Learning: Classification - <strong>IBM</strong>
-    <a href="https://coursera.org/share/aff5958c63d29045ea6705c5f53e7312" target="_blank" class="certificate-link">Certificate</a>
-  </li>
-  <li>
-    Supervised Machine Learning: Regression - <strong>IBM</strong>
-    <a href="https://coursera.org/share/eb908b4c7de3f10029cdb0f830a615e5" target="_blank" class="certificate-link">Certificate</a>
-  </li>
-  <li>
-    Unsupervised Machine Learning - <strong>IBM</strong>
-    <a href="https://coursera.org/share/2cbfa3d073a657eeb81d648d01f70b86" target="_blank" class="certificate-link">Certificate</a>
-  </li>
-  <li>
-    Introduction to TensorFlow for AI, ML, and DL - <strong>DeepLearning.ai</strong>
-    <a href="https://coursera.org/share/1c13e75dc0557eede49d5cc017d850de" target="_blank" class="certificate-link">Certificate</a>
-  </li>
-  <li>
-    Introduction to Machine Learning - <strong>IIT Kharagpur</strong>
-  </li>
-        </ul>
-      </div>
       <div className="experience-education">
       
       {/* Education Section */}
